c89b9e65db8ccdc2217b84377797575942689d9b
vkaides <aorata0903@gmail.com>
2020-12-04 23:17:55 -0500

    fix commit

---
+++ server/routes/users.js
@@ -0,0 +1,15 @@
+/*
+Author: Kenneth Agustin
+Team: Team Seven
+Course: COMP229 - Fall 2020
+Purpose: Team Project - Survey Site
+*/ 
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('Placeholder');
+});
+
+module.exports = router;
